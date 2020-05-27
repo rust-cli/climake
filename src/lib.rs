4dc4a2220f9e7916778ad96c7552e49fc235eb53
Owez <owez@scalist.net>
2020-05-27 18:27:36 +0100

    Added help messages

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,11 @@
 //! For more infomation, please see the [CLIMake] object and [Argument] to get
 //! started parsing arguments using this library.
 
-mod parse;
-pub use parse::*;
+use std::env;
 
 /// The way the argument is called, can short or long. This enum is made to be
 /// used in a [Vec] as then you may have multiple ways to call it.
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub enum CliCallType {
     /// Short call only, for example the `h` in `-hijk`.
     Short(char),
@@ -20,12 +20,13 @@ pub enum CliCallType {
 }
 
 /// A single argument in a list of arguments to parse in [CliMake].
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct CliArgument {
     /// The way(s) in which you call this argument, used internally.
     pub calls: Vec<CliCallType>,
 
     /// Optional inner-command help.
-    pub help: String,
+    pub help_str: String,
 }
 
 impl CliArgument {
@@ -44,39 +45,106 @@ impl CliArgument {
         if help.is_some() {
             return CliArgument {
                 calls: calls,
-                help: help.unwrap(),
+                help_str: help.unwrap(),
             };
         }
 
         CliArgument {
             calls: calls,
-            help: String::from("No extra CLI help provided."),
+            help_str: String::from("No extra CLI help provided."),
         }
     }
 }
 
 /// Main holder structure of entire CliMake library.
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct CliMake {
     /// Arguments that this library parses.
     pub arguments: Vec<CliArgument>,
 
+    /// Name of CLI displayed on help page.
+    pub name: String,
+
     /// Help message, optionally provided by user.
-    pub help: String,
+    pub help_str: String,
 }
 
 impl CliMake {
     /// Creates a new [CliMake] from arguments and optional help.
-    pub fn new(arguments: Vec<CliArgument>, help: Option<String>) -> Self {
+    pub fn new(arguments: Vec<CliArgument>, name: String, help: Option<String>) -> Self {
         if help.is_some() {
             return CliMake {
                 arguments: arguments,
-                help: help.unwrap(),
+                name: name,
+                help_str: help.unwrap(),
             };
         }
 
         CliMake {
             arguments: arguments,
-            help: String::from("No extra argument help provided."),
+            name: name,
+            help_str: String::from("No extra argument help provided."),
         }
+    }
+
+    /// Adds new argument to [CliMake]
+    pub fn add_arg(&mut self, argument: CliArgument) {
+        self.arguments.push(argument);
+    }
+
+    /// Returns parsed help message as a [String].
+    pub fn help_msg(&self) -> String {
+        let cur_exe = env::current_exe();
+
+        let mut arg_help = String::new();
+
+        for arg in self.arguments.clone() {
+            let mut arg_vec = Vec::new();
+
+            for call in arg.calls {
+                match call {
+                    CliCallType::Long(l) => arg_vec.push(format!("--{}", l)),
+                    CliCallType::Short(s) => arg_vec.push(format!("-{}", s)),
+                }
+            }
+
+            arg_help.push_str(&format!("{} | {}\n", arg_vec.join(", "), arg.help_str));
+        }
+
+        format!(
+            "Usage: ./{} [OPTIONS]\n\n  {}\n\nOptions:\n{}",
+            cur_exe.unwrap().file_stem().unwrap().to_str().unwrap(),
+            self.help_str,
+            arg_help
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Ensures help message displays without errors.
+    #[test]
+    fn help_msg() {
+        let cli_args = vec![
+            CliArgument::new(
+                vec!['q', 'r', 's'],
+                vec![String::from("hi"), String::from("second")],
+                None,
+            ),
+            CliArgument::new(
+                vec!['a', 'b', 'c'],
+                vec![String::from("other"), String::from("thing")],
+                None,
+            ),
+        ];
+        let cli = CliMake::new(
+            cli_args,
+            String::from("Test CLI"),
+            Some(String::from("A simple CLI.")),
+        );
+
+        cli.help_msg();
     }
 }
--- src/parse.rs
+++
@@ -1,7 +0,0 @@
-//! DEV: Contains [nom]-based parsing internals.
-
-use crate::CliMake;
-use std::env;
-
-/// Parses a [String] using the provided [CliMake] schema.
-pub fn parse_str(input: String, cli: CliMake) {}
