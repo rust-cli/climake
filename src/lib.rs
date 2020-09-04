31fceaf4f2bce0dda2ad51db988cfeea4da60ef1
Owez <root@ogriffiths.com>
2020-09-04 01:14:27 +0100

    Removed old "errors on x" notice (no longer valid) & doc example

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,30 @@
 //!
 //! ## Example 📚
 //!
-//! Rewrite example coming soon!
+//! ```should_panic
+//! use climake::{Argument, CLIMake, DataType};
+//!
+//! fn main() {
+//!     let args = vec![
+//!         Argument::new(
+//!             vec!['o'],
+//!             vec![String::from("output"), String::from("out")],
+//!             Some("Example output arg"),
+//!             DataType::File,
+//!         ),
+//!         Argument::new(
+//!             vec!['a', 'b', 'c'],
+//!             vec![],
+//!             Some("Alphabet!"),
+//!             DataType::None,
+//!         ),
+//!     ];
+//!
+//!     let cli = CLIMake::new(args, Some("A showcase CLI to demonstrate climake"), None);
+//!
+//!     println!("Args used:\n{:#?}", cli.parse());
+//! }
+//! ```
 //!
 //! ## Installation 🚀
 //!
@@ -46,10 +69,10 @@ pub enum DataType {
     /// will return nothing
     None,
 
-    /// Plaintext (typically used), will return a [String]. Errors if no data is passed
+    /// Plaintext (typically used), will return a [String]
     Text,
 
-    /// A file or directory, will return a [PathBuf]. Errors if no data is passed
+    /// A file or directory, will return a [PathBuf]
     File,
 }
 
