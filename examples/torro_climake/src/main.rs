b4c82c260c61af54d1a7b9b5e42e55d982fab123
Owez <root@ogriffiths.com>
2020-09-13 23:39:37 +0100

    Added torro_climake example

---
+++ examples/torro_climake/src/main.rs
@@ -0,0 +1,38 @@
+use climake::{Argument, CLIMake, DataType, PassedData};
+use torro::torrent::Torrent;
+
+fn main() {
+    let torrent_arg = Argument::new(
+        vec!['t', 'f'],
+        vec![String::from("torrent"), String::from("file")],
+        Some("A .torrent file to download"),
+        DataType::File,
+    )
+    .unwrap();
+
+    let cli = CLIMake::new(
+        vec![torrent_arg.clone()],
+        Some("OTorrent, a micro torrent client"),
+        None,
+    )
+    .unwrap();
+
+    let mut torrent_arg_buf = None;
+
+    for used_arg in cli.parse() {
+        if used_arg.argument == torrent_arg {
+            torrent_arg_buf = Some(used_arg.passed_data)
+        }
+    }
+
+    match torrent_arg_buf {
+        Some(torrent_arg_data) => match torrent_arg_data {
+            PassedData::File(file) => println!(
+                "Torrent creation result: {:?}",
+                Torrent::from_file(file[0].clone())
+            ),
+            _ => panic!(), // should never happen unless climake screws up
+        },
+        None => panic!("OTorrent has not implemented this error yet!"),
+    }
+}
