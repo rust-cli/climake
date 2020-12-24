fb7d21ef553dbd38b1f771579a37396503af63a0
Owez <root@ogriffiths.com>
2020-12-24 03:38:27 +0000

    Refractored

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,22 @@
 //!
 //! - [Crates.io](https://crates.io/crates/climake)
 //! - [Documentation](https://docs.rs/climake)
-//! 
+//!
 //! # Example 📚
-//! 
+//!
 //! Rewrite example coming soon!
-//! 
+//!
 //! ## Installation 🚀
-//! 
+//!
 //! Simply add the following to your `Cargo.toml` file:
-//! 
+//!
 //! ```toml
 //! [dependencies]
 //! climake = "3.0" # note: rewrite isn't out just yet!
 //! ```
-//! 
+//!
 //! # License
-//! 
+//!
 //! Duel-licensed under both the [MIT License](https://opensource.org/licenses/MIT)
 //! ([`LICENSE-MIT`](LICENSE-MIT)) and [Apache 2.0 License](https://www.apache.org/licenses/LICENSE-2.0)
 //! ([`LICENSE-APACHE`](LICENSE-APACHE)), you may choose at your discretion.
